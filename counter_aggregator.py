f6a2a08940778c90d6eacca474ab87d94d0d0029
Vincent Dupont <vincent@tsc.ai>
2019-09-29 21:19:13 +0800

    Milestone Report: Exploratory analysis

--- counter_aggregator.py
+++ counter_aggregator.py
@@ -1,22 +1,30 @@
 import sys
 from collections import Counter
+from datetime import datetime
 from glob import glob
 
 
-def monogram_counter_aggregator(pathname):
-    files = glob(pathname)
+def counter_aggregator(output_file, input_files):
+    files = glob(input_files)
     words_count = Counter()
     for file in files:
-        print('processing file {}'.format(file))
-        for line in open(file).readlines():
-            word, count = line.strip().split(',')
-            words_count += Counter({word: int(count)})
+        total_lines = len(open(file).readlines())
+        with open(file) as input_file:
+            line_number = 0
+            previous_percentage = 0
+            for line in input_file.readlines():
+                line_number += 1
+                current_percentage = int(line_number/total_lines*100)
+                if current_percentage > previous_percentage:
+                    previous_percentage = current_percentage
+                    print('[{}] File {} {}% processed'.format(datetime.now(), file, current_percentage))
+                word, count = line.strip().split(',')
+                words_count += Counter({word: int(count)})
 
-    # TODO filename in parameter
-    # with open('datasets/monogram_frequencies.txt', 'w') as f:
-    #     for word, count in words_count.most_common():
-    #         f.write('{},{}\n'.format(word, count))
+    with open(output_file, 'w') as f:
+        for word, count in words_count.most_common():
+            f.write('{},{}\n'.format(word, count))
 
 
 if __name__ == "__main__":
-    monogram_counter_aggregator(sys.argv[1])
+    counter_aggregator(sys.argv[1], sys.argv[2])
