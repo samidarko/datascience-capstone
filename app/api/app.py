46ce5454a73420fb900815ffc327eafcd1ceee4b
samidarko <samidarko@gmail.com>
2019-10-17 18:20:13 +0800

    feat: added words model to backend

--- app/api/app.py
+++ app/api/app.py
@@ -1,9 +1,12 @@
+import pandas as pd
 from flask import Flask, request
 
 app = Flask(__name__)
 
 # monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)]
 # monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)].ngram.values[:10]
+words = pd.read_csv('vocab.csv', names=['words'])
+
 
 @app.route('/', methods=['POST'])
 def predict():
