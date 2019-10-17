873818d47ec462bd2b60bf3c0fcd62732611e909
samidarko <samidarko@gmail.com>
2019-10-17 18:16:55 +0800

    web app back-end base

---
+++ app/api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)]
+# monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)].ngram.values[:10]
+
+@app.route('/', methods=['POST'])
+def predict():
+    print(request.get_json())
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
