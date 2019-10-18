d3eb33782b4ed81fa33439d741adda7eb5aaf6cb
samidarko <samidarko@gmail.com>
2019-10-18 14:34:23 +0800

    feat: added monograms and bigrams models to backend

--- app/api/app.py
+++ app/api/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 # monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)]
 # monogram_frequencies_df[monogram_frequencies_df.ngram.str.startswith('t', na=False)].ngram.values[:10]
 words = pd.read_csv('vocab.csv', names=['words'])
+monograms = pd.read_csv('monograms.csv', names=['monogram', 'prediction'])
+bigrams = pd.read_csv('bigrams.csv', names=['bigram', 'prediction'])
 
 
 @app.route('/', methods=['POST'])
